44b7c34bcc34a9c24f8f13bfdcbdbdb26aee0cab
Rodrigo Roberto de Almeida <rodrigo.roberto.almeida@gmail.com>
2021-11-24 18:51:59 -0300

    created dashboard routes

--- app/server/data/dashboardData.ts
+++ app/server/data/dashboardData.ts
@@ -4,19 +4,19 @@ import database from "../infra/database";
 export default class DashboardData {
   public async getTodayPending(companyName: string): Promise<Schedule | void> {
     var utc = new Date().toJSON().slice(0,10).replace(/-/g,'/');
-    const query = `select * from public."schedule" WHERE "companyName" LIKE '%${companyName}%' AND "status" = '0' AND datetime LIKE '${utc}%'`;
+    const query = `select * from public."schedule" INNER JOIN public."users" ON (public."schedule"."userId" = public."users"."userid") WHERE "companyName" LIKE '%${companyName}%' AND "status" = '0' AND datetime LIKE '${utc}%'`;
     return await database.query(query);
   }
 
   public async getTodayEntrance(companyName: string): Promise<Schedule | void> {
     var utc = new Date().toJSON().slice(0,10).replace(/-/g,'/');
-    const query = `select * from public."schedule" WHERE "companyName" LIKE '%${companyName}%' AND "status" = '1' AND datetime LIKE '${utc}%'`;
+    const query = `select * from public."schedule" INNER JOIN public."users" ON (public."schedule"."userId" = public."users"."userid") WHERE "companyName" LIKE '%${companyName}%' AND "status" = '1' AND datetime LIKE '${utc}%'`;
     return await database.query(query);
   }
 
   public async getTodaySchedule(companyName: string): Promise<Schedule | void> {
     var utc = new Date().toJSON().slice(0,10).replace(/-/g,'/');
-    const query = `select * from public."schedule" WHERE "companyName" LIKE '%${companyName}%' AND datetime LIKE '${utc}%'`;
+    const query = `select * from public."schedule" INNER JOIN public."users" ON (public."schedule"."userId" = public."users"."userid") WHERE "companyName" LIKE '%${companyName}%' AND datetime LIKE '${utc}%'`;
     return await database.query(query);
   }
 }
