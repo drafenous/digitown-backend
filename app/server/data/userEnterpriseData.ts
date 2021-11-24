1e00dd3092612150562d74a943b572e2d91d8f9a
Rodrigo Roberto de Almeida <rodrigo.roberto.almeida@gmail.com>
2021-11-24 01:59:14 -0300

    trying to fix table name

--- app/server/data/userEnterpriseData.ts
+++ app/server/data/userEnterpriseData.ts
@@ -4,7 +4,7 @@ import database from "../infra/database";
 
 export default class UserEnterpriseData {
   public async getUser(email: string, password: string): Promise<UserEnterprise | void> {
-    const query = `select * from "public.enterpriseUsers" where email = '${email}'`;
+    const query = `select * from public."enterpriseUsers" where email = '${email}'`;
     return await database.query(query).then((users: UserEnterprise[]) =>
       users.find(async (user) => {
         return await bcrypt.compare(password, user.passwordHash);
@@ -14,7 +14,7 @@ export default class UserEnterpriseData {
 
   public async createUser({fullName, email, passwordHash: password, enterpriseId}: UserEnterprise): Promise<UserEnterprise | void> {
     const hash = await bcrypt.hash(password, 10);
-    const query = `INSERT INTO "public.enterpriseUsers"(
+    const query = `INSERT INTO public."enterpriseUsers"(
       "fullName", email, "passwordHash", "enterpriseId")
       VALUES ('${fullName}', '${email}', '${hash}', '${enterpriseId}');`;
       return await database.query(query)
